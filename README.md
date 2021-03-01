e7ff14cf96a70a9c34038cb41d053ba5ba42aa60
JonanthaW <jonanthawillian@gmail.com>
2021-02-28 22:05:56 -0500

    Added light theme

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ Um gerador RGB, colocando a data e Horário atual, utilizando **HTML, JS, CSS**.
 ### Tela da aplicação
 
 ![image](https://github.com/JonanthaW/Projeto-DevChallengeGeradorRGB/blob/main/assets/example1.jpg)
+![image](https://github.com/JonanthaW/Projeto-DevChallengeGeradorRGB/blob/main/assets/example2.jpg)
--- content/js/main.js
+++ content/js/main.js
@@ -1,6 +1,19 @@
 var getNow = () => {
   let date = new Date()
   document.querySelector(".hourContainer p").innerText = `${date.getDay().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}/${date.getMonth().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}/${date.getFullYear()} - ${date.getHours().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}:${date.getMinutes().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}`
+
+  if (date.getHours() >= 6 && date.getHours <= 18) {
+    document.querySelector("section").style.backgroundColor = "#ffffff";
+    document.querySelector(".hourContainer").style.backgroundColor = "#f6f6f6";
+    document.querySelector(".RGBContainer").style.backgroundColor = "#e5e5e5";
+    document.querySelectorAll('h3, h4, p, i').forEach(e => e.style.color = "black");
+  }
+  else {
+    document.querySelector("section").style.backgroundColor = "#373c44";
+    document.querySelector(".hourContainer").style.backgroundColor = "#44474e";
+    document.querySelector(".RGBContainer").style.backgroundColor = "#44474e";
+    document.querySelectorAll('h3, h4, p, i').forEach(e => e.style.color = "white");
+  }
 }
 getNow();
 
